d304bec595050a26efa9b462547e53ff6f025128
Team Tinitiate / Venkata Bhattaram <tinitiate@users.noreply.github.com>
2023-05-22 13:15:56 -0400

    Update s3_download_upload.py

--- s3_download_upload.py
+++ s3_download_upload.py
@@ -2,21 +2,32 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-# params = config('config.ini','aws')
-# profile_name = params.get('profile')
-
-
+# Set up AWS session
 boto3.setup_default_session(profile_name='sbadmin')
 s3_client = boto3.client('s3')
 
-def download_s3_file(p_s3_bucket_name, p_s3_folder,p_s3_file_name,p_local_download_folder_file):
+def download_s3_file(p_s3_bucket_name, p_s3_folder, p_s3_file_name, p_local_download_folder_file):
+    """
+    Download a file from AWS S3.
 
-    l_s3_file_path_name = p_s3_folder+"/"+p_s3_file_name
+    :param p_s3_bucket_name: Name of the S3 bucket.
+    :param p_s3_folder: Path to the folder in S3 where the file is located.
+    :param p_s3_file_name: Name of the file in S3.
+    :param p_local_download_folder_file: Local file path where the downloaded file will be saved.
+    """
+    l_s3_file_path_name = p_s3_folder + "/" + p_s3_file_name
     s3_client.download_file(p_s3_bucket_name, l_s3_file_path_name, p_local_download_folder_file)
 
+def upload_my_file(p_s3_bucket_name, p_s3_folder, p_local_folder_file_to_upload, p_upload_file_name="N/A"):
+    """
+    Upload a file to AWS S3.
 
-def upload_my_file(p_s3_bucket_name, p_s3_folder, p_local_folder_file_to_upload, p_upload_file_name = "N/A"):
-
+    :param p_s3_bucket_name: Name of the S3 bucket.
+    :param p_s3_folder: Path to the folder in S3 where the file will be uploaded.
+    :param p_local_folder_file_to_upload: Local file path of the file to be uploaded.
+    :param p_upload_file_name: Optional. Name of the file in S3. If not provided, the local file name will be used.
+    :return: True if the upload is successful, False otherwise.
+    """
     l_s3_file_name = ""
     
     if p_upload_file_name == "N/A":
@@ -33,6 +44,7 @@ def upload_my_file(p_s3_bucket_name, p_s3_folder, p_local_folder_file_to_upload,
         print(e)
         return False
     return True
+
 
 def main():
     l_s3_bucket='sb-events-s3-bucket'
